5acf17fc143a397ec3459fa7e047ec7985f8326e
Esneider M <esneidermanuelbuelvas1997@gmail.com>
2021-09-15 16:50:02 -0500

    completar actualizar

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const express = require('express');
 const app = express(); //se instancia objeto
 const http = require('http'); //para peticiones http
 const server = http.createServer(app);
+const { Server } = require('socket.io');//aca se solicita la libreria cocket.io que previamente inistaladas con npm install
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+      }
+});//se crea un servicor interno para las peticiones 
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+    
+  socket.on('socket update', (data) => {
+      console.log(data);
+      io.emit('socket update', data);
+  });
+
+  socket.on('login', () => {
+      io.emit('login', 'conectado');
+  });
+
 });
 
 server.listen(3000, () => {
--- web/js/clientes/clientes.js
+++ web/js/clientes/clientes.js
@@ -1,29 +1,46 @@
-
 //main execute
 $(document).ready(function(){
     //alert('cualquier cosa');
     listar();
 });
 
-// funcion guardar post
-function guardar() {
-    var nombre   =   $('#nombre').val();
-    var apellido =   $('#apellido').val();
-    var telefono =   $('#telefono').val();
-    var correo   =   $('#correo').val();
-
+// FUNCION GUARDAR
+function guardar(){
+    var nombre= $('#nombre').val();
+    var apellido = $('#apellido').val();
+    var telefono= $('#telefono').val();
+    var correo=$('#correo').val();
     console.log(nombre, apellido, telefono, correo);
 
     const data = JSON.stringify(
-        {
-            'nombre': nombre,
-            'apellido': apellido,
-            'correo': correo,
-            'telefono': telefono
-        }
+            {
+                'nombre': nombre,
+                'apellido':apellido,
+                'telefono':telefono,
+                'correo':correo
+            }
     );
-    
+    //Mostrar objeto
+    console.log(data);
+
+    $.ajax({
+        url: 'http://localhost/App/api/Clientes/create',
+        type:'POST',
+        data:data,
+        success: function(respuesta) {
+           console.log(respuesta)
+           socket.emit('socket update', respuesta);
+        },
+        error: function() {
+            console.log("No se ha podido obtener la información");
+        },complete:function(){
+            var formulario =document.getElementById("f1");
+            formulario.reset();
+        }
+    });
 }
+
+
 //funcion listar datos de la tabla el orden lo decide el controlador 
 function listar(){
     let swich=0;
@@ -35,7 +52,7 @@ function listar(){
                 $.each(respuesta, function(i, item){
                     if(swich<1){
                         arreglo=Object.keys(item);
-                    console.log(arreglo);
+                    /* console.log(arreglo); */
                     swich = 1;
                     }
                     arreglo=Object.values(item);
@@ -53,6 +70,67 @@ function listar(){
         },
         error: function() {
             console.log("No se ha podido obtener la información");
+        }
+    });
+}
+
+function pasarparametros(nombre,apellido,telefono,correo, id){
+    $('#nombre').val(nombre);
+    $('#apellido').val(apellido);
+    $('#telefono').val(telefono);
+    $('#correo').val(correo);
+    $('#id').val(id);
+    $("#btn-guardar").attr('onclick','actualizar()');    
+}
+
+function eliminar(id){
+    console.log(id);
+     $.ajax({
+        url: 'http://localhost/App/api/Clientes/delete/'+id,
+        type:'get',
+        success: function(respuesta) {
+           console.log(respuesta)
+           socket.emit('socket update', respuesta);
+        },
+        error: function() {
+            console.log("No se pudo eliminar registro");
+        }
+    });
+}
+
+function actualizar() {
+    var nombre   = $('#nombre').val();
+    var apellido = $('#apellido').val();
+    var telefono = $('#telefono').val();
+    var correo   = $('#correo').val();
+    var id       = $('#id').val();
+    
+    console.log(nombre, apellido, telefono, correo);
+
+    const data = JSON.stringify(
+        {
+            'nombre'  :   nombre,
+            'apellido':   apellido,
+            'telefono':   telefono,
+            'correo'  :   correo,
+            'id'      :   id
+        }
+    );
+
+    $.ajax({
+        url: 'http://localhost/App/api/update/'+id,
+        type:'POST',//tipo de servicio a consultar
+        data:data,//dato opcional que se envia al servicio
+        success: function(respuesta) {
+            console.log(respuesta)
+            socket.emit('socket update', respuesta);//en el caso de usar socket.io
+        },
+        error: function() {
+            console.log('');//si se desea enviar respuesta a consola del error
+        },complete:function(){
+            $("btn-guardar").attr('onclik', 'actualizar()');
+            var formulario = document.getElementById("f1");
+            formulario.reset();
         }
     });
 }
