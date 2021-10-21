f745514603c34520ab5f68412a92a077276e032b
Esneider M <esneidermanuelbuelvas1997@gmail.com>
2021-10-20 19:11:12 -0500

    decodificado token

--- web/js/usuarios/usuarios.js
+++ web/js/usuarios/usuarios.js
@@ -1,4 +1,4 @@
-console.log('Funcionando...');
+/* history.forward(); */
 
 function ingresar() {
 
@@ -7,20 +7,29 @@ function ingresar() {
 
     console.log(usuario, contrasena );
 
-    const data = JSON.stringify(
+    const datos = JSON.stringify(
         {
             usuario,
             contrasena
         }
     );
-    
+
     $.ajax({
         url: 'http://localhost/App/auth/login2',
         type:'post',
-        data: data,
+        data: datos,
         success: function(respuesta) {
-            console.log(respuesta)
-            /* window.location = 'http://localhost/medintegral/App/'; */
+            console.log( parseJwt(respuesta.Token).data);
+            if(JSON.stringify(parseJwt(respuesta.Token).data.estado == '1')){
+
+                console.log(parseJwt(respuesta.Token).data.estado == '1');
+                window.sessionStorage.setItem("usuario",  JSON.stringify(parseJwt(respuesta.Token).data));
+                window.location = 'http://localhost/medintegral/App/';
+                
+            }else{
+                alert('Usuario no esta activo')
+            }            
+            
         },
         error: function() {
             console.log('Error de ingreso');//si se desea enviar respuesta a consola del error
@@ -29,6 +38,46 @@ function ingresar() {
             formulario.reset();
         }
     })
-    
-    
 }
+
+
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
+
+
+/* var inactivityTime = function () {
+    var time;
+    window.onload = resetTimer;
+    // DOM Events
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+    document.onload = resetTimer;
+    document.onmousemove = resetTimer;
+    document.onmousedown = resetTimer; // touchscreen presses
+    document.ontouchstart = resetTimer;
+    document.onclick = resetTimer;     // touchpa
+
+
+ document.onscroll = resetTimer;    // scrolling with arrow keys
+    document.onkeypress = resetTimer;
+
+    function logout() {
+        if (sessionStorage.getItem("usuario") !== null) {
+            $("#modal-servicios").modal('hide');
+            $("#modal-login").modal('show');
+            $("#mensage-error_login").empty().append(`🅴🆇🅿🅸🆁🅾 🆃🅸🅴🅼🅿🅾 🅳🅴 🅸🅽🅰🅲🆃🅸🆅🅸🅳🅰🅳.`);
+        }
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(logout, 3000)
+        // 1000 milliseconds = 1 second
+    }
+} */
